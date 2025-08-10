b90f465bfdbed09f7f80098aa4056a03a3a6708f
AlfredChester <fredbao1126@gmail.com>
2025-08-10 23:56:11 +0800

    Calcite icon.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import HelloWorld from "./components/HelloWorld.vue";
-import DuckChess from "./components/DuckChess.vue";
 </script>
 
 <template>
@@ -11,8 +10,11 @@ import DuckChess from "./components/DuckChess.vue";
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+    <a href="https://calcite.alfredbao.cn/" target="_blank">
+      <img src="./assets/calcite.svg" class="logo vue" alt="Calcite logo" />
+    </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue + Calcite" />
   <DuckChess />
 </template>
 
